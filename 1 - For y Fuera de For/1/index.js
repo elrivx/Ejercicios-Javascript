a9deb2854929d8281a360acf1880e370053cdb73
elRivx <elrivx@gmail.com>
2021-04-05 15:19:56 -0400

    Primer ejercicio.
    Diferencia entre tener una impresión por consola fuera o dentro del bucle FOR.

---
+++ 1 - For y Fuera de For/1/index.js
@@ -0,0 +1,11 @@
+// Imprimir el valor de i
+
+
+for (var i=0; i<10; i++){
+
+    // Cada vez que el bucle For cumpla su objetivo, imprimirá el
+    // valor de i
+
+    console.log(i);
+
+}
---
+++ 1 - For y Fuera de For/2/index.js
@@ -0,0 +1,14 @@
+// Se imprimirá el valor de x
+
+var x = 1;
+
+for (var i=0; i<3; i++){
+
+    // Hacemos la suma
+    x = x+1;
+
+}
+
+// Al no estar dentro del bucle FOR, sólo imprimirá
+// el último valor que tenga x en la suma.
+console.log(x);
