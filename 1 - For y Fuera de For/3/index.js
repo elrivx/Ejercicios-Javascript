218a1dd27908f4df0df5cab91ea3ddd4c34c5504
elRivx <elrivx@gmail.com>
2021-04-05 15:54:11 -0400

    Ejercicio 3 de la parte 1.

---
+++ 1 - For y Fuera de For/3/index.js
@@ -0,0 +1,19 @@
+// Se imprimirá el valor de x y el valor de la variable
+// interna i del bucle FOR.
+
+var x = 1;
+
+for (var i=0; i<3; i++){
+
+    // Hacemos la suma
+    x = x+1;
+
+    // Imprimirá la suma
+    console.log(x);
+    // Imprimirá el valor de i en cada vuelta
+    console.log(i);
+
+}
+
+
+
