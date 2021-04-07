dc0fd2dfd89ed39c34ecae5f4150c854eae14b3d
elRivx <elrivx@gmail.com>
2021-04-07 00:17:48 -0400

    Repitiendo un For

---
+++ 01 - For y Fuera de For/4/index.js
@@ -0,0 +1,14 @@
+// Aquí hay un caso particular.
+// Se podría decir que el primer FOR va a repetir 3 veces el
+// segundo bucle FOR
+
+for (var num1=1; num1<=3; num1++){
+
+
+    for (var num2=1; num2 <=2; num2++){
+
+        console.log('Dojo');
+
+    }
+
+}
