727e0be26a27a89a3dd457cc36b41bca152c1bf0
elRivx <elrivx@gmail.com>
2021-04-06 23:47:57 -0400

    "Quebremos" la tensión.

---
+++ 03 - Quebremos la tensión/index.js
@@ -0,0 +1,18 @@
+// ¿Qué pasaría si pones break en una instrucción?
+
+
+for (var i=1; i<=20; i++){
+
+    if (i >5){
+
+        break;
+    }
+    else
+    {
+        console.log('Hello');
+    }
+}
+
+// El break se usa para terminar un bucle de forma inmediata.
+// En el ejercicio, terminará el bucle FOR cuando
+// i sea mayor que 5.
